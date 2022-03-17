aee722863b41e9b647e771fc42cd5d6b66bd0c18
Fernando Kenji Sakabe <fernandoks@al.insper.edu.br>
2022-03-17 14:59:38 -0300

    preparando cabeca

---
+++ Assets/Scripts/Follower.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour
+{
+    [SerializeField] GameObject hasToFollow;
+
+    void Update()
+    {
+        transform.position = hasToFollow.transform.position;
+        transform.rotation = hasToFollow.transform.rotation;
+    }
+}
