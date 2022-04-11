3a601c20c3f5e4ab80e0684b36285f9e9ca2283e
Fernando Kenji Sakabe <fernandoks@al.insper.edu.br>
2022-04-11 16:59:00 -0300

    Saving feet position and rotation on file after quitting unity

--- Assets/Scripts/RecPositions.cs
+++ Assets/Scripts/RecPositions.cs
@@ -8,19 +8,27 @@ public class RecPositions : MonoBehaviour {
      public float tSample = 10.0f; // sampling starts after this time
      private List<Vector3> positions;
      private List<Vector3> rotations;
+     private List<Vector3> rpositions;
+     private List<Vector3> rrotations;
  
      GameObject MLeftFoot;
+     GameObject MRightFoot;
  
      void Start(){
          MLeftFoot = GameObject.Find("MLeftFoot");
+         MRightFoot = GameObject.Find("MRightFoot");
          positions = new List<Vector3>(); // initialize the array...
          rotations = new List<Vector3>();
+         rpositions = new List<Vector3>(); // initialize the array...
+         rrotations = new List<Vector3>();
          // and start recording after tSample:
          InvokeRepeating("RecPoint", tSample, interval);
      }
      void RecPoint(){
          positions.Add(MLeftFoot.transform.position);
          rotations.Add(MLeftFoot.transform.eulerAngles);
+         rpositions.Add(MRightFoot.transform.position);
+         rrotations.Add(MRightFoot.transform.eulerAngles);
          // store position...
      }
      // function that saves to a text file:
@@ -29,7 +37,7 @@ public class RecPositions : MonoBehaviour {
          
          for(int i=0; i<positions.Count; i++){
             
-            string line = System.String.Format("{0:f2},{1:f2},{2:f2},{3:f2};{4:f2};{5:f2}\r\n", positions[i].x, positions[i].y, positions[i].z, rotations[i].x, rotations[i].y, rotations[i].z);
+            string line = System.String.Format("{0:f2},{1:f2},{2:f2},{3:f2};{4:f2};{5:f2};{6:f2},{7:f2},{8:f2},{9:f2};{10:f2};{11:f2}\r\n", positions[i].x, positions[i].y, positions[i].z, rotations[i].x, rotations[i].y, rotations[i].z, rpositions[i].x, rpositions[i].y, rpositions[i].z, rrotations[i].x, rrotations[i].y, rrotations[i].z);
             System.IO.File.AppendAllText(fileName, line); // append to the file
          }
     }
