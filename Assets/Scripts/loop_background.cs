18c59c65e2abf6222d845b59c6d03dd35ddaf675
Fernando Kenji Sakabe <fernandoks@al.insper.edu.br>
2022-07-29 13:43:06 -0300

    Fatia adicionada

---
+++ Assets/Scripts/loop_background.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroll : MonoBehaviour {
+
+   public float scroll_Speed = 0.1f;
+   
+   private MeshRenderer mesh_Renderer;
+
+    void Awake  () {
+        mesh_Renderer = GetComponent<MeshRenderer>(); 
+    }
+
+    void Update () {
+
+        float x = Time.time * scroll_Speed;
+
+        Vector2 offset = new Vector2(x, 0);
+
+        mesh_Renderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+   
+   
+    }
+
+
+
+}  // Class
