098782f79410e35e579417310ec12d7ccc735a73
Fernando Kenji Sakabe <fernandoks@al.insper.edu.br>
2022-08-09 16:09:17 -0300

    mudanca para quaternion

--- Assets/Scripts/MyBehaviour.cs
+++ Assets/Scripts/MyBehaviour.cs
@@ -31,25 +31,27 @@ public class MyBehaviour : MonoBehaviour
         {
 
             string uri = "http://127.0.0.1:8000/";
-
+            float fx = foot.position.x;
             float fy = foot.position.y;
             float fz = foot.position.z;
             float frx= foot.rotation.x;
             float fry= foot.rotation.y;
             float frz= foot.rotation.z;
+            float frw = foot.rotation.w;
             float rfy = rfoot.position.y;
             float rfz = rfoot.position.z;
             float rfrx= rfoot.rotation.x;
             float rfry= rfoot.rotation.y;
             float rfrz= rfoot.rotation.z;
+            float rfrw= rfoot.rotation.w;
             
-            string envio = System.String.Format("[{0},{1},{2},{3},{4},{5}]", frx, fry, frz, rfrx, rfry, rfrz);  
+            string envio = System.String.Format("[{0},{1},{2},{3},{4},{5},{6},{7}]", frx, fry, frz, frw, rfrx, rfry, rfrz, rfrw);  
             
             //uri += "[0.08,%200.14,%200.09,%200.18]";
 
             uri += envio;
 
-            yield return new WaitForSeconds(0.015f);
+            yield return new WaitForSeconds(0.1f);
 
             using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))    
             {
--- Assets/Scripts/RecPositions.cs
+++ Assets/Scripts/RecPositions.cs
@@ -3,15 +3,15 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 public class RecPositions : MonoBehaviour {
-     public string fileName = "walk_data_5_Daniel.txt"; // file pathname
+     public string fileName = "walk_data_7_Daniel.txt"; // file pathname
      public float interval = 0.1f; // save positions each 0.1 second
      public float tSample = 60; // sampling starts after this time
      private List<Vector3> positions;
-     private List<Vector3> rotations;
+     private List<Quaternion> rotations;
      private List<Vector3> rpositions;
-     private List<Vector3> rrotations;
+     private List<Quaternion> rrotations;
      private List<Vector3> cpositions;
-     private List<Vector3> crotations;
+     private List<Quaternion> crotations;
  
      GameObject MLeftFoot;
      GameObject MRightFoot;
@@ -22,21 +22,21 @@ public class RecPositions : MonoBehaviour {
          MRightFoot = GameObject.Find("MRightFoot");
          Camera= GameObject.Find("Camera");
          positions = new List<Vector3>(); // initialize the array...
-         rotations = new List<Vector3>();
+         rotations = new List<Quaternion>();
          rpositions = new List<Vector3>(); // initialize the array...
-         rrotations = new List<Vector3>();
+         rrotations = new List<Quaternion>();
          cpositions = new List<Vector3>(); // initialize the array...
-         crotations = new List<Vector3>();
+         crotations = new List<Quaternion>();
          // and start recording after tSample:
          InvokeRepeating("RecPoint", tSample, interval);
      }
      void RecPoint(){
          positions.Add(MLeftFoot.transform.position);
-         rotations.Add(MLeftFoot.transform.eulerAngles);
+         rotations.Add(MLeftFoot.transform.rotation);
          rpositions.Add(MRightFoot.transform.position);
-         rrotations.Add(MRightFoot.transform.eulerAngles);
+         rrotations.Add(MRightFoot.transform.rotation);
          cpositions.Add(Camera.transform.position);
-         crotations.Add(Camera.transform.eulerAngles);
+         crotations.Add(Camera.transform.rotation);
          // store position...
      }
      // function that saves to a text file:
@@ -44,8 +44,7 @@ public class RecPositions : MonoBehaviour {
          System.IO.File.WriteAllText(fileName, ""); // clear old file, if any
          
          for(int i=0; i<positions.Count; i++){
-            
-            string line = System.String.Format("{0:f2},{1:f2},{2:f2},{3:f2},{4:f2},{5:f2},{6:f2},{7:f2},{8:f2},{9:f2},{10:f2},{11:f2},{12:f2},{13:f2},{14:f2},{15:f2},{16:f2},{17:f2}\r\n", positions[i].x, positions[i].y, positions[i].z, rotations[i].x, rotations[i].y, rotations[i].z, rpositions[i].x, rpositions[i].y, rpositions[i].z, rrotations[i].x, rrotations[i].y, rrotations[i].z, cpositions[i].x, cpositions[i].y, cpositions[i].z, crotations[i].x, crotations[i].y, crotations[i].z);
+            string line = System.String.Format("{0:f2},{1:f2},{2:f2},{3:f2},{4:f2},{5:f2},{6:f2},{7:f2},{8:f2},{9:f2},{10:f2},{11:f2},{12:f2},{13:f2},{14:f2},{15:f2},{16:f2},{17:f2},{18:f2},{19:f2},{20:f2}\r\n", positions[i].x, positions[i].y, positions[i].z, rotations[i].x, rotations[i].y, rotations[i].z, rotations[i].w, rpositions[i].x, rpositions[i].y, rpositions[i].z, rrotations[i].x, rrotations[i].y, rrotations[i].z, rrotations[i].w, cpositions[i].x, cpositions[i].y, cpositions[i].z, crotations[i].x, crotations[i].y, crotations[i].z, crotations[i].w);
             System.IO.File.AppendAllText(fileName, line); // append to the file
          }
     }
